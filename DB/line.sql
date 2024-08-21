766e39bbf718f8c4484febc5826ee6c18f417bb5
kee236 <31869356+kee236@users.noreply.github.com>
2024-08-22 01:49:38 +0700

    Update DB

---
+++ DB/line.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS `line_users_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `line_user_id` varchar(200) NOT NULL,
+  `display_name` varchar(200) DEFAULT NULL,
+  `picture_url` text DEFAULT NULL,
+  `status_message` varchar(255) DEFAULT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `line_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `app_name` varchar(255) NOT NULL,
+  `channel_id` varchar(255) NOT NULL, 
+  `channel_secret` varchar(255) NOT NULL,
+  `channel_access_token` varchar(255) NOT NULL, 
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
